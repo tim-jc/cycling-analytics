5c48b3264ff2e87060033536cae25c4251b6741b
tim-jc <tim_cross@me.com>
2026-07-02 20:41:01 +0100

    db connection sorted

--- db/db.R
+++ db/db.R
@@ -1,41 +1,38 @@
 library(DBI)
 library(RMariaDB)
-library(dotenv)
 
-connect_db <- function() {
-  load_dot_env()
+connect_db <- function(max_attempts = 5, wait_seconds = 30) {
+  for (i in seq_len(max_attempts)) {
+    cat(glue::glue(
+      "DB connection attempt {i}/{max_attempts}\n"
+    ))
 
-  con <- dbConnect(
-    RMariaDB::MariaDB(),
-    host = Sys.getenv("DB_HOST"),
-    port = as.integer(Sys.getenv("DB_PORT")),
-    dbname = Sys.getenv("DB_NAME"),
-    user = Sys.getenv("DB_USER"),
-    password = Sys.getenv("DB_PASSWORD")
-  )
+    con <- tryCatch(
+      dbConnect(
+        RMariaDB::MariaDB(),
+        host = Sys.getenv("MARIADB_HOST"),
+        port = as.integer(Sys.getenv("MARIADB_PORT")),
+        dbname = Sys.getenv("MARIADB_NAME"),
+        user = Sys.getenv("MARIADB_USER"),
+        password = Sys.getenv("MARIADB_PASSWORD")
+      ),
 
-  return(con)
-}
+      error = function(e) {
+        cat("Connection failed:\n")
+        cat(conditionMessage(e), "\n")
 
-with_db_connection <- function(code_block) {
-  con <- NULL
-
-  tryCatch(
-    {
-      con <- connect_db()
-
-      return(code_block(con))
-    },
-    error = function(e) {
-      cat("ERROR:\n")
-      cat(e$message, "\n")
-
-      return(NULL)
-    },
-    finally = {
-      if (!is.null(con)) {
-        dbDisconnect(con)
+        NULL
       }
+    )
+
+    if (!is.null(con)) {
+      cat("DB connection successful.\n")
+
+      return(con)
     }
-  )
+
+    Sys.sleep(wait_seconds)
+  }
+
+  stop("Unable to connect to database.")
 }
